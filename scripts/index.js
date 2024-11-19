e0f97115df75a54c33ef0a9a5573ca9dd2a9511b
ian <iancane001@gmail.com>
2024-11-19 16:58:25 -0300

    imgs

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,6 @@
 'use strict';
 
 /*
- * APELLIDO, NOMBRE | APELLIDO, NOMBRE
+ * CANÉ IAN
  */
 
-// Podria levantar los productos de un JSON o directamente estar escritos aca
-
-let productos = [
-    // {
-    //     id: 1,
-    //     nombre: 'Producto',
-    //     descripcion: 'Descripción del producto',
-    //     precio: 0,
-    //     imagen: 'images/producto-de-ejemplo.jpg',
-    //     categoría: 'Nombre de la categoría',
-    // }
-];
